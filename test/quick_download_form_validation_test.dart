0c2ca3a546d52c3ed58ce4358833d7b13670ea53
tsunehimatoi <tsunehimatoi@gmail.com>
2026-08-21 21:48:20 +0800

    test(ui): adapt quick download validation coverage (adapted from 75e2d1d)

--- test/quick_download_form_validation_test.dart
+++ test/quick_download_form_validation_test.dart
@@ -13,9 +13,6 @@ class _FakeHost implements QuickDownloadFormHost {
   List<QuickQueueOption> get queues => const [];
 
   @override
-  List<QuickDeviceOption> get devices => const [];
-
-  @override
   int get defaultSegments => 0;
 
   @override
